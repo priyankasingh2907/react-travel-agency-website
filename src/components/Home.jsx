6e93ceaec8543c9616cef6317c6da1bd7c3550ba
priyanka singh <priyanka.singh.2907.2k.com>
2024-07-11 00:45:28 +0530

    Home page created

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,3 +1,111 @@
+import styled from "styled-components";
+import Hero from "../components/assets/hero.png";
+import { useState } from "react";
+import Button from "./Button";
+
 export default function Home() {
-  return <div>Home</div>;
+  const [value, setValue] = useState("$5000-$10000");
+  return (
+    <Section>
+      <div className="background">
+        <img src={Hero} alt="hero" />
+      </div>
+      <div className="content">
+        <div className="info">
+          <h1>Its Time To </h1>
+          <h1>Explore the World</h1>
+          <Button text="Plan Your Trip" />
+        </div>
+        <div className="planner">
+          <form>
+            <div className="row">
+              <label htmlFor="Destinations">Destinations</label>
+              <select name="Destinations" id="Destinations">
+                <option value="Arab">Arab Egypt</option>
+                <option value="Udaipur">Udaipur India</option>
+              </select>
+            </div>
+            <div className="row">
+              <label htmlFor="">Check In</label>
+              <input type="date" name="" id="" />
+            </div>
+            <div className="row">
+              <label htmlFor="">Price Range</label>
+              <input
+                type="text"
+                value={value}
+                onChange={(e) => setValue(e.target.value)}
+              />
+            </div>
+            <div className="row">
+              <Button text="Discover More" />
+            </div>
+          </form>
+        </div>
+      </div>
+    </Section>
+  );
 }
+const Section = styled.section`
+  margin-top: 2rem;
+  position: relative;
+  .background {
+    img {
+      height: 90vh;
+      width: 100%;
+    }
+  }
+  .content {
+    .info {
+      position: absolute;
+      top: 3rem;
+      margin-left: 7rem;
+      h1 {
+        font-size: 4rem;
+        margin-bottom: 2rem;
+      }
+    }
+    .planner {
+      position: absolute;
+      bottom: 2rem;
+      right: 0;
+      background-color: #f5e9e9;
+      padding: 1rem;
+      box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+      form {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        gap: 3rem;
+        .row {
+          display: flex;
+          flex-direction: column;
+          text-align: start;
+          label {
+            font-size: 0.7rem;
+            color: var(--secondary-text);
+          }
+          input,
+          select {
+            border: none;
+            width: 100%;
+            color: var(--primary-color);
+            margin-top: 0.5rem;
+            background-color: white;
+            font-size: 1.1rem;
+            border-bottom: 1px;
+            padding-bottom: 0.3rem;
+          }
+          input:focus {
+            outline: none;
+          }
+          input[type="date"]::-webkit-calander-picker-indicator {
+            cursor: pointer;
+            filter: invert(58%) sepia(69%) saturate(2588%) hue-rotate(325deg)
+              brightness(105%) contrast(101%);
+          }
+        }
+      }
+    }
+  }
+`;
